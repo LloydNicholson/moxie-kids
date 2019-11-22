1886b94f91931e9bf3614b314a051f07e374d54a
Lloyd Nicholson <nichlloyd@gmail.com>
2019-11-22 15:57:43 +0200

    initial commit

---
+++ wp-content/plugins/woocommerce-payfast-gateway/readme.txt
@@ -0,0 +1,47 @@
+=== WooCommerce PayFast Gateway ===
+Contributors: automattic, royho, akeda, mattyza, bor0, woothemes, dwainm, laurendavissmith001
+Tags: credit card, payfast, payment request, woocommerce, automattic
+Requires at least: 4.4
+Tested up to: 5.3
+Requires PHP: 5.6
+Stable tag: 1.4.14
+License: GPLv3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+
+This is the official WooCommerce extension to receive payments using the South African PayFast payments provider.
+
+== Description ==
+
+The PayFast extension for WooCommerce enables you to accept payments including Subscriptions, Deposits & Pre-Orders via one of South Africa’s most popular payment gateways.
+
+= Why choose PayFast? =
+
+PayFast gives your customers more flexibility including putting down deposits, ordering ahead of time or paying on a weekly, monthly or annual basis.
+
+== Frequently Asked Questions ==
+
+= Does this require a PayFast merchant account? =
+
+Yes! A PayFast merchant account, merchant key and merchant ID are required for this gateway to function.
+
+= Does this require an SSL certificate? =
+
+An SSL certificate is recommended for additional safety and security for your customers.
+
+= Where can I find documentation? =
+
+For help setting up and configuring, please refer to our [user guide](https://docs.woocommerce.com/document/payfast-payment-gateway)
+
+= Where can I get support or talk to other users? =
+
+If you get stuck, you can ask for help in the Plugin Forum.
+
+== Changelog ==
+
+= 1.4.14 - 2019-10-24
+ * Fix   - Incorrect API response handling for subscription renewal payments.
+ * Tweak - WC tested up to 3.8
+ * Tweak - WP tested up to 5.3
+
+[See changelog for all versions](https://raw.githubusercontent.com/woocommerce/woocommerce-gateway-payfast/master/changelog.txt).
+
---
+++ wp-content/themes/moxiekids-storefront-child/footer.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package storefront
+ */
+
+//include CSS Style Sheet
+echo "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\">";
+?>
+
+</div><!-- .col-full -->
+</div><!-- #content -->
+
+<footer id="colophon" class="moxie-footer site-footer" role="contentinfo">
+    <div class="col-full">
+        <h3 class="aligncenteruppercase">Moxie Kids Craft Box</h3>
+        <div style="min-width: 100%; width: 100%;">
+            <div class="alignleft">
+                <h4>FIND YOUR WAY AROUND</h4>
+                <ul style="list-style: none; margin-left: 10px">
+                    <li><a href="/basket">My Cart</a></li>
+                    <li><a href="/my-account">Log In</a></li>
+                    <li><a href="">FAQs</a></li>
+                    <li><a href="">Terms + Conditions</a></li>
+                    <li><a href="/privacy-policy">Privacy Policy</a></li>
+                </ul>
+            </div>
+            <div class="alignright">
+                <h4>LET’S CHAT!</h4>
+                <ul style="list-style: none; margin-left: 10px">
+                    <li><a href="/contact">Contact Us</a></li>
+                    <li>+27 83 444 6081</li>
+                    <li><i class="fa fa-facebook-official"></i> <a href="https://www.facebook.com/moxiekidscraftbox">moxiekidscraftbox</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </div><!-- .col-full -->
+
+    <div>
+        <p style="text-align: center; font-family: Futura, serif">&copy; Moxie Kids 2019</p>
+    </div>
+</footer><!-- #colophon -->
+
+</div><!-- #page -->
+
+</body>
+</html>
